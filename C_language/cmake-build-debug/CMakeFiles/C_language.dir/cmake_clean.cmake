6a6d1b239a663290cfa1904e24e59f65c57cdfe2
cxj <chenxiangjun19931227@gmail.com>
2017-03-17 13:26:54 +0800

    2017/3/16 CCF

--- C_language/cmake-build-debug/CMakeFiles/C_language.dir/cmake_clean.cmake
+++ C_language/cmake-build-debug/CMakeFiles/C_language.dir/cmake_clean.cmake
@@ -1,10 +1,5 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/C_language.dir/14.cpp.o"
-  "C_language.pdb"
-  "C_language"
-)
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang )
   include(CMakeFiles/C_language.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
