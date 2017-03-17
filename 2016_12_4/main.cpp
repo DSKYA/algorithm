772a7954c06eacf3be34bf9114e20277903a5278
cxj <chenxiangjun19931227@gmail.com>
2017-03-17 21:37:14 +0800

    2016年12月CCF

--- 2016_12_4/main.cpp
+++ 2016_12_4/main.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
+#include <stdio.h>
+#define MAX 99999999999999
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    long long a[1002][1002];
+    long long b[1002][1002];
+    int n;
+    scanf("%d",&n);
+    for (int i = 1; i <= n; ++i) {
+        long long tmp;
+        scanf("%lld",&tmp);
+        a[i][i] = tmp;
+        b[i][i] = tmp;
+    }
+    for(int i = 1; i <= n; i++){
+        for (int j = 1; j <= n - i; ++j) {
+            long long max = MAX;
+            b[j][j + i] = b[j][j + i - 1] + b[j + i][j + i];
+            for (int k = 0; k < i; ++k) {
+                long long sum = 0;
+                sum += b[j][j + k];
+                if(k > 0)sum += a[j][j + k];
+                sum += b[j + k +1][j + i];
+                if(k < i - 1)sum += a[j + k + 1][j + i];
+                if(sum < max)max = sum;
+            }
+            a[j][j + i] = max;
+        }
+    }
+    printf("%lld",a[1][n]);
     return 0;
 }
---
+++ 2016_9_1/cmake-build-debug/CMakeFiles/2016_9_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2016_9_1.dir/main.cpp.o"
+  "2016_9_1.pdb"
+  "2016_9_1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2016_9_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
